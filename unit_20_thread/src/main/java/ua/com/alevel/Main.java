3984439d917471e6ee4bcc36e21f0318c71d38bc
egorfuntusov <funtushan@gmail.com>
2022-01-19 12:28:38 +0200

    add sync, callable and future

--- unit_20_thread/src/main/java/ua/com/alevel/Main.java
+++ unit_20_thread/src/main/java/ua/com/alevel/Main.java
@@ -1,8 +1,55 @@
 package ua.com.alevel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
 
-    public static void main(String[] args) {
-        ProgramRun.run();
+    public static void main(String[] args) throws Exception {
+//        ProgramRun.run();
+        parallelSum();
+    }
+
+    private static void parallelSum() throws Exception {
+        Integer[] arr = MathUtil.randomArrays(1_000_000, 1, 1_000_000);
+        Integer[][] arrr = MathUtil.divideArr(arr);
+        Long sum = 0L;
+        for (Integer[] integers : arrr) {
+            System.out.println("next");
+            Callable<Long> callable = () -> {
+                Thread.sleep(2000);
+                return MathUtil.sum(integers);
+            };
+            sum += callable.call();
+        }
+        System.out.println("sum = " + sum);
+
+        sum = 0L;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(arrr.length);
+        List<Future<Long>> futures = new ArrayList<>();
+        for (Integer[] integers : arrr) {
+            System.out.println("next");
+            Future<Long> future = executorService.submit(() -> {
+                Thread.sleep(2000);
+                return MathUtil.sum(integers);
+            });
+            futures.add(future);
+        }
+
+        while (true) {
+            if (futures.stream().allMatch(Future::isDone)) {
+                for (Future<Long> future : futures) {
+                    sum += future.get();
+                }
+                break;
+            }
+        }
+
+        System.out.println("sum = " + sum);
     }
 }
